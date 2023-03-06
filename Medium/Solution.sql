857d6e3497582f24e4cea30c396da16492e65365
Anish Shakya <shakyaanish69@gmail.com>
2023-03-06 17:49:50 +0545

    Medium problems added

--- Medium/Solution.sql
+++ Medium/Solution.sql
@@ -1,111 +1,135 @@
 -----MEDIUM Problems solutions
 
----1.	Show unique birth years from patients and order them by ascending.
-select distinct(Year(birth_date))
-from patients
-order by birth_date
+---1.	Show unique birth YEARs FROM patients and order them by ascending.
+SELECT DISTINCT(YEAR(birth_date))
+FROM patients
+ORDER BY birth_date
 
----2.	Show unique first names from the patients table which only occurs once in the list.
+---2.	Show unique first names FROM the patients table which only occurs once in the list.
 --------For example, if two or more people are named 'John' in the first_name column then don't include their name in the output list. If only 1 person is named 'Leo' then include them in the output.
-select first_name
-from patients
-group by first_name
-having count(first_name) = 1
+SELECT first_name
+FROM patients
+GROUP BY first_name
+HAVING COUNT(first_name) = 1
 
----3.	Show patient_id and first_name from patients where their first_name start and ends with 's' and is at least 6 characters long.
-select patient_id,first_name
-from patients
-where first_name like 's%' and first_name like '%s' and len(first_name) >= 6
+---3.	Show patient_id and first_name FROM patients WHERE their first_name start and ends with 's' and is at least 6 characters long.
+SELECT patient_id,first_name
+FROM patients
+WHERE first_name like 's%' and first_name like '%s' and len(first_name) >= 6
 
----4.	Show patient_id, first_name, last_name from patients whos diagnosis is 'Dementia'.
+---4.	Show patient_id, first_name, last_name FROM patients whos diagnosis is 'Dementia'.
 ---Primary diagnosis is stored in the admissions table.
-select p.patient_id,p.first_name,p.last_name
-from patients p 
+SELECT p.patient_id,p.first_name,p.last_name
+FROM patients p 
 join admissions a on p.patient_id = a.patient_id
-where a.diagnosis = 'Dementia'
+WHERE a.diagnosis = 'Dementia'
 
 ---5.	Display every patient's first_name.
 ---Order the list by the length of each name and then by alphabetically
-select first_name
-from patients
-order by  len(first_name) ,first_name
+SELECT first_name
+FROM patients
+ORDER BY  len(first_name) ,first_name
 
 ---6.	Show the total amount of male patients and the total amount of female patients in the patients table.
 ----Display the two results in the same row.
-select
-sum	(case when gender ='M' then 1 else 0 end	)AS male_count,
-sum(case when gender ='F' then 1 else 0 end )as female_count
-from patients
+SELECT
+SUM	(case when gender ='M' then 1 else 0 end	)AS male_COUNT,
+SUM(case when gender ='F' then 1 else 0 end )as female_COUNT
+FROM patients
 
----7.	Show first and last name, allergies from patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
-select first_name,last_name,allergies
-from patients
-where allergies in ('Penicillin','Morphine')
-order by allergies,first_name,last_name
+---7.	Show first and last name, allergies FROM patients which have allergies to either 'Penicillin' or 'Morphine'. Show results ordered ascending by allergies then by first_name then by last_name.
+SELECT first_name,last_name,allergies
+FROM patients
+WHERE allergies in ('Penicillin','Morphine')
+ORDER BY allergies,first_name,last_name
 
----8.	Show patient_id, diagnosis from admissions. Find patients admitted multiple times for the same diagnosis.
-select f.patient_id,f.diagnosis
-from(
-select patient_id,diagnosis
-from admissions
-group by patient_id,diagnosis
-having count(*) > 1) f
+---8.	Show patient_id, diagnosis FROM admissions. Find patients admitted multiple times for the same diagnosis.
+SELECT f.patient_id,f.diagnosis
+FROM(
+SELECT patient_id,diagnosis
+FROM admissions
+GROUP BY patient_id,diagnosis
+HAVING COUNT(*) > 1) f
 
 ---9.	Show the city and the total number of patients in the city.
----Order from most to least patients and then by city name ascending.
-select city,count(*) as total_patient
-from patients
-group by city
-order by total_patient desc , city asc
+---Order FROM most to least patients and then by city name ascending.
+SELECT city,COUNT(*) as total_patient
+FROM patients
+GROUP BY city
+ORDER BY total_patient desc , city asc
 
 ---10.	Show first name, last name and role of every person that is either patient or doctor.
 ---The roles are either "Patient" or "Doctor"
-select first_name,last_name,'Patient' AS role from patients
+SELECT first_name,last_name,'Patient' AS role FROM patients
 union all 
-select first_name,last_name,'Doctor' As role from doctors
+SELECT first_name,last_name,'Doctor' As role FROM doctors
 
----11.	Show all allergies ordered by popularity. Remove 'NKA' and NULL values from query.
-select allergies , count(* ) as total_diagnosis
-from patients
-where allergies is not null
-group by allergies
-order by total_diagnosis desc
+---11.	Show all allergies ordered by popularity. Remove 'NKA' and NULL values FROM query.
+SELECT allergies , COUNT(* ) as total_diagnosis
+FROM patients
+WHERE allergies is not null
+GROUP BY allergies
+ORDER BY total_diagnosis desc
 
----12.	Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting from the earliest birth_date.
-select first_name,last_name,birth_date
-from patients
-where  year(birth_date) between 1970 and 1979
-order by birth_date
+---12.	Show all patient's first_name, last_name, and birth_date who were born in the 1970s decade. Sort the list starting FROM the earliest birth_date.
+SELECT first_name,last_name,birth_date
+FROM patients
+WHERE  YEAR(birth_date) between 1970 and 1979
+ORDER BY birth_date
 
 ---13.	We want to display each patient's full name in a single column. Their last_name in all upper letters must appear first, then first_name in all lower case letters. Separate the last_name and first_name with a comma. Order the list by the first_name in decending order
 ---EX: SMITH,jane
-select concat(upper(last_name),',',lower(first_name)) AS new_name_format
-from patients
-order by first_name desc
+SELECT concat(upper(last_name),',',lower(first_name)) AS new_name_format
+FROM patients
+ORDER BY first_name desc
 
----14.	Show the province_id(s), sum of height; where the total sum of its patient's height is greater than or equal to 7,000.
-select province_id ,sum(height)
-from patients
-group by province_id
-having sum(height) >=7000
+---14.	Show the province_id(s), SUM of height; WHERE the total SUM of its patient's height is greater than or equal to 7,000.
+SELECT province_id ,SUM(height)
+FROM patients
+GROUP BY province_id
+HAVING SUM(height) >=7000
 
 ---15.	Show the difference between the largest weight and smallest weight for patients with the last name 'Maroni'
-select (max(weight) - min(weight)) As weight_delta
-from patients
-where last_name = 'Maroni'
+SELECT (MAX(weight) - MIN(weight)) As weight_delta
+FROM patients
+WHERE last_name = 'Maroni'
 
 ---16.	Show all of the days of the month (1-31) and how many admission_dates occurred on that day. Sort by the day with most admissions to least admissions.
-select day(admission_date) as day_number,
-count(admission_date) as number_of_admission
-from admissions
-group by day_number
-order by number_of_admission desc
+SELECT DAY(admission_date) as day_number,
+COUNT(admission_date) as number_of_admission
+FROM admissions
+GROUP BY day_number
+ORDER BY number_of_admission desc
 
 ---17.	Show the all columns for patient_id 542's most recent admission_date.
-select * 
-from admissions
-where patient_id = 542
-order by admission_date desc
-limit 1
+SELECT * 
+FROM admissions
+WHERE patient_id = 542
+ORDER BY admission_date desc
+LIMIT 1
 
-------The End----------------------------------------------------------------------
+----- 18) Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria:
+---1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+---2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+SELECT patient_id,attending_doctor_id,diagnosis
+FROM admissions
+WHERE (patient_id % 2 != 0  AND   attending_doctor_id IN (1,5,19))
+	OR
+    ( len(patient_id) = 3  AND attending_doctor_id like '%2%' )
+    
+---19) Show first_name, last_name, and the total number of admissions attended for each doctor.
+-------Every admission has been attended by a doctor.
+SELECT d.first_name,d.last_name,COUNT(a.patient_id) As TotalAdmission
+FROM admissions a 
+INNER JOIN doctors d ON d.doctor_id = a.attending_doctor_id
+GROUP BY d.first_name,d.last_name
+
+
+---20)For each doctor, display their id, full name, and the first and last admission date they attended.
+SELECT d.doctor_id,concat(d.first_name,' ',d.last_name) AS full_name,
+	MIN(admission_date) As first_admission_date ,MAX(admission_date) AS last_admission_date
+FROM admissions a 
+JOIN doctors d ON a.attending_doctor_id = d.doctor_id
+GROUP BY d.doctor_id
+
+
